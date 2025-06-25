6c2ae868119fae474721f6e392d780c1e1b0d838
cherrin1 <cherrin@connexpay.com>
2025-06-25 08:34:19 -0500

    Update events.js

--- api/slack/events.js
+++ api/slack/events.js
@@ -1,17 +1,24 @@
 export default function handler(req, res) {
+  console.log('Method:', req.method);
+  console.log('Body:', req.body);
+  
   if (req.method === 'GET') {
     return res.status(200).send('Slack events endpoint is alive.');
   }
 
   if (req.method === 'POST') {
     const { type, challenge } = req.body;
+    
+    console.log('Type:', type);
+    console.log('Challenge:', challenge);
 
     if (type === 'url_verification') {
+      console.log('Sending challenge back:', challenge);
       return res.status(200).send(challenge);
     }
 
-    return res.status(200).end();
+    return res.status(200).json({ ok: true });
   }
 
-  res.status(405).send('Method Not Allowed');
+  return res.status(405).send('Method Not Allowed');
 }
